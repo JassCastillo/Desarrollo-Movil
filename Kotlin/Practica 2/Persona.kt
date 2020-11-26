7a03e461a53646004997983bbaeb4bd3799cff6d
JassCastillo <jasmillponce35@yahoo.com>
2020-11-26 16:01:38 -0600

    Clase personas modificada

--- Kotlin/Practica 2/Persona.kt
+++ Kotlin/Practica 2/Persona.kt
@@ -1,20 +1,20 @@
-class Persona{
-    var nombre:String = ""
-    var apellido:String = ""
-    constructor(){
-    }
-    constructor(n:String, a:String):this()
-    {
-    nombre=n
-    apellido=a
-    }
-   }
-   fun main(){
-    var persona = Persona("Yesser","Miranda")
-    var nueva = Persona()
-    nueva.nombre = "Juan"
-    nueva.apellido = "Lopez"
-    println(persona.nombre)
-    println(nueva.nombre)
-   }
-   
+/*class Persona{
+ var nombre:String = ""
+ var apellido:String = ""
+ constructor(){
+ }
+ constructor(n:String, a:String):this()
+ {
+ nombre=n
+ apellido=a
+ }*/
+ class Persona(var nombre:String, var apellido:String)
+
+fun main(){
+ var persona = Persona("Yesser","Miranda")
+ //var nueva = Persona()
+ //nueva.nombre = "Juan"
+ //nueva.apellido = "Lopez"
+ println(persona.nombre)
+ //println(nueva.nombre)
+}
