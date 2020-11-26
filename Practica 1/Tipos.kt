76f12a9581244cbc6eddb1c545c87bb54ea2cf57
JassCastillo <jasmillponce35@yahoo.com>
2020-11-25 20:31:10 -0600

    ejercicios de tipos

---
+++ Practica 1/Tipos.kt
@@ -0,0 +1,17 @@
+fun main()
+{
+//numeros
+val int = 123
+val long = 123456L
+val double = 12.34
+val float = 12.34F
+val hexadecimal = 0xAB
+val binary = 0b01010101
+ 
+    println(int)
+    println(long)
+    println(double)
+    println(float)
+    println(hexadecimal)
+    println(binary)
+}
---
+++ Practica 1/Variables.kt
@@ -0,0 +1,20 @@
+fun main()
+{
+
+    var nombre = "Gerardo"
+    nombre = "Josue"
+
+    println("Mi nombre es $nombre")
+
+    var apellido = "Castillo"
+    apellido = "Morales"
+
+    println("Mi apellido es $apellido")
+
+    var edad = "21"
+
+    println("Mi edad es $edad")
+
+    println("Mi nombre es $nombre $apellido y tengo $edad años")
+}
+
